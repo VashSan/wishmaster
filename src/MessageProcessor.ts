6353eac2b1ee42dbab90f03c799d3476838c4805
Alexander Schwoch <alex@palc.de>
2018-02-13 21:13:45 +0100

    added tslint and linted MessageProcessor

--- src/MessageProcessor.ts
+++ src/MessageProcessor.ts
@@ -2,7 +2,7 @@ import * as Collections from 'typescript-collections';
 
 export interface IPlugin {
     trigger: string;
-    act(message: Message) : BotAnswer;
+    act(message: Message): BotAnswer;
 }
 
 export class BotAnswer {
@@ -14,12 +14,12 @@ export class Message {
     from: string;
     channel: string;
 
-    constructor(init?:Partial<Message>) {
+    constructor(init?: Partial<Message>) {
         (<any>Object).assign(this, init);
     }
 
-    toString() : string{
-        let result : string = `Message from '${this.from}' to '${this.channel}': ${this.text}`;
+    toString(): string {
+        let result: string = `Message from '${this.from}' to '${this.channel}': ${this.text}`;
         return result;
     }
 }
@@ -30,23 +30,23 @@ export class MessageProcessor {
     plugins = new Collections.LinkedList<IPlugin>();
 
     constructor() {
-        
+
     }
 
-    add(message : Message){ 
+    add(message: Message) {
         this.messageQueue.enqueue(message);
-        if (this.messageQueue.size() % this.messageProcessingThreshold == 0){
+        if (this.messageQueue.size() % this.messageProcessingThreshold == 0) {
             this.processMessages();
         }
     }
 
-    registerPlugin(plugin : IPlugin){
-        if(plugin.trigger != null){
+    registerPlugin(plugin: IPlugin) {
+        if (plugin.trigger != null) {
             plugin.trigger = plugin.trigger.toLowerCase();
         }
-        
-        this.plugins.forEach(function(p){
-            if(p.trigger == plugin.trigger){
+
+        this.plugins.forEach(function (p) {
+            if (p.trigger == plugin.trigger) {
                 throw "Can not add plugin because same trigger exists already";
             }
         });
@@ -54,14 +54,13 @@ export class MessageProcessor {
         this.plugins.add(plugin);
     }
 
-    private processMessages(){
+    private processMessages() {
 
-        while(this.messageQueue.size() > 0)
-        {
+        while (this.messageQueue.size() > 0) {
             let msg = this.messageQueue.dequeue();
-            this.plugins.forEach(function(p){ 
+            this.plugins.forEach(function (p) {
                 let words = msg.text.split(" ", 1);
-                if(p.trigger == null || words[0].toLowerCase() == p.trigger){
+                if (p.trigger == null || words[0].toLowerCase() == p.trigger) {
                     p.act(msg);
                 }
             });
