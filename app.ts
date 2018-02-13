a57274db9ea1b68ab4913a38f51b57779c87b7e9
Alexander Schwoch <alex@palc.de>
2018-02-13 21:14:29 +0100

    first time connection

--- app.ts
+++ app.ts
@@ -1,22 +1,56 @@
-import * as mp from "./src/MessageProcessor";
-import {Loopback} from "./src/Plugins/Loopback";
+import * as MP from "./src/MessageProcessor";
+import { Loopback } from "./src/Plugins/Loopback";
+import * as IRC from "irc";
+import { Configuration } from "./src/Configuration";
 
 //import * as Collections from "typescript-collections";
 
 class Startup {
     public static main(): number {
-        
-        let proc = new mp.MessageProcessor();
-        let plg : mp.IPlugin = new Loopback(null);
 
-        proc.registerPlugin( plg );
+        let config = new Configuration();
 
-        let m1 : mp.Message = new mp.Message({ channel: "1", from: "a", text: "asd1" });
-        let m2 : mp.Message = new mp.Message({ channel: "1", from: "a", text: "asd2" });
-        let m3 : mp.Message = new mp.Message({ channel: "1", from: "a", text: "asd3" });
-        let m4 : mp.Message = new mp.Message({ channel: "1", from: "a", text: "asd4" });
-        let m5 : mp.Message = new mp.Message({ channel: "1", from: "a", text: "asd5" });
-        let m6 : mp.Message = new mp.Message({ channel: "1", from: "a", text: "asd6" });
+        let client = new IRC.Client(
+            config.server,
+            config.nickname,
+            {
+                autoConnect: false,
+                password: config.password
+            }
+        );
+
+
+        client.addListener("raw", message => {
+            console.log("raw: ", message);
+        });
+
+        client.addListener("error", message => {
+            console.log("error: ", message);
+        });
+
+        client.addListener("message", (from, to, message) => {
+            console.log(from + " => " + to + ": " + message);
+        });
+
+        client.connect(() => {
+
+            client.join(config.channel);
+
+            client.say(config.channel, "I'm a bot!");
+
+        });
+
+        let proc = new MP.MessageProcessor();
+        let plg: MP.IPlugin = new Loopback(null);
+
+        proc.registerPlugin(plg);
+
+        let m1: MP.Message = new MP.Message({ channel: "1", from: "a", text: "asd1" });
+        let m2: MP.Message = new MP.Message({ channel: "1", from: "a", text: "asd2" });
+        let m3: MP.Message = new MP.Message({ channel: "1", from: "a", text: "asd3" });
+        let m4: MP.Message = new MP.Message({ channel: "1", from: "a", text: "asd4" });
+        let m5: MP.Message = new MP.Message({ channel: "1", from: "a", text: "asd5" });
+        let m6: MP.Message = new MP.Message({ channel: "1", from: "a", text: "asd6" });
 
         console.log(m1.toString());
 
