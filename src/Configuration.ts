1b070f017d928b6926384f05ef9fb95a9b493318
Alexander Schwoch <alex@palc.de>
2018-02-13 21:14:05 +0100

    added Configuration class

---
+++ src/Configuration.ts
@@ -0,0 +1,31 @@
+import fs = require("fs");
+
+export class Configuration {
+    private configDir: string;
+    private configFile: string = "wishmaster.json";
+    private configFilePath: string;
+
+    server: string;
+    nickname: string;
+    password: string;
+    channel: string;
+
+    constructor() {
+        this.configDir = `${process.env.localappdata}\\.wishmaster`;
+        this.configFilePath = `${this.configDir}\\${this.configFile}`;
+
+        if (!fs.existsSync(this.configDir)) {
+            fs.mkdirSync(this.configDir);
+
+            fs.writeFileSync(this.configFilePath,
+                `{"server": "", "nickname": "", "password": "", "channel": ""}`);
+        }
+
+        let configFile = fs.readFileSync(this.configFilePath);
+        let configString = configFile.toString("utf8");
+        let configObj = JSON.parse(configString);
+
+        (<any>Object).assign(this, configObj);
+    }
+
+}
