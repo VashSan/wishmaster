6e7ae6e11190e20265357105d23a7205e834588c
Alexander Schwoch <alex@palc.de>
2020-01-11 21:19:22 +0100

    added possibility to switch scenes

--- src/shared/ObsController.ts
+++ src/shared/ObsController.ts
@@ -1,13 +1,14 @@
 import * as OBSWebSocket from "obs-websocket-js";
-import { Context } from ".";
-import { Configuration, IObsConfig } from "./Configuration";
 import { ILogger } from "psst-log";
+
+import { IObsConfig } from "./Configuration";
 
 export class ObsController {
     config: IObsConfig | null;
     obs: OBSWebSocket = new OBSWebSocket();
     isConnected: boolean = false;
     log: ILogger;
+    availableScenes: Map<string, OBSWebSocket.Scene> = new Map<string, OBSWebSocket.Scene>();
 
     constructor(obsConfig: IObsConfig | null, logger: ILogger) {
         this.config = obsConfig;
@@ -23,6 +24,27 @@ export class ObsController {
         }).then(() => {
             this.log.info("Connected to OBS.");
             this.isConnected = true;
+            return this.obs.send('GetSceneList', undefined);
+        }).then((data: any) => {
+            this.log.info(`Scanning ${data.scenes.length} available scenes!`);
+
+            data.scenes.forEach((scene: OBSWebSocket.Scene) => {
+                this.availableScenes.set(scene.name, scene);
+            });
+        }).catch(err => {
+            this.log.error("Error connecting and retreiving Scens from OBS: " + err);
         });
+    }
+
+    public switchToScene(sceneName: string) {
+        if (this.isConnected && this.availableScenes.has(sceneName)) {
+            this.obs.send('SetCurrentScene', {
+                'scene-name': sceneName
+            }).catch(err => {
+                this.log.error(`Could not switch to scene ${sceneName}. ${err}`);
+            });
+        } else {
+            this.log.error(`Could not switch to scene ${sceneName}. It must exist on bot startup.`);
+        }
     }
 }
