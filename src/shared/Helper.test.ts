f1c3047288e5b9c705a417be289c16124d79ad5b
Alexander Schwoch <alex@palc.de>
2020-02-02 21:27:47 +0100

    added ignore during timeout helper

--- src/shared/Helper.test.ts
+++ src/shared/Helper.test.ts
@@ -1,7 +1,46 @@
 import { Seconds } from "./";
+import { IgnoreDuringTimeout } from "./Helper";
 describe('seconds', () => {
     test('milliseconds', () => {
         let result = new Seconds(1).inMilliseconds();
         expect(result).toBe(1000);
     });
+});
+
+describe('IgnoreDuringTimeout', () => {
+    test('handler is invoked', () => {
+        // Arrange
+        let handlerWasInvoked = false;
+        let handler = new IgnoreDuringTimeout(new Seconds(0.5), null, () => {
+            handlerWasInvoked = true;
+        });
+
+        // Act
+        handler.handle();
+
+        // Assert
+        expect(handlerWasInvoked).toBe(true);
+    });
+
+    test('handler can be invoked after expiration', (done) => {
+        // Arrange
+        let handlerInvokeCount = 0;
+        let handler = new IgnoreDuringTimeout(new Seconds(0.5), null, () => {
+            handlerInvokeCount += 1;
+        });
+
+        // Act
+        handler.handle();
+        handler.handle(); // will be ignored
+
+        setTimeout(()=>{
+            handler.handle(); // will be invoked again
+
+            // Assert
+            expect(handlerInvokeCount).toBe(2);
+            done();
+        }, new Seconds(0.6).inMilliseconds());
+
+    });
+
 });
--- src/shared/Helper.ts
+++ src/shared/Helper.ts
@@ -8,3 +8,33 @@ export class Seconds {
         return this.seconds * 1000;
     }
 }
+
+/** calls a handler only if the timeout expired */
+export class IgnoreDuringTimeout<T> {
+    private readonly timeout: Seconds;
+    private readonly argument: T;
+    private readonly handler: (arg: T) => void;
+    private lastCall: Date | null = null;
+
+    constructor(timeout: Seconds, argument: T, handler: (arg: T) => void) {
+        this.timeout = timeout;
+        this.argument = argument;
+        this.handler = handler;
+    }
+
+    handle(): void {
+        if (this.hasTimeoutExpired()) {
+            this.lastCall = new Date();
+            this.handler(this.argument);
+        }
+    }
+
+    private hasTimeoutExpired(): boolean {
+        if (this.lastCall == null) {
+            return true;
+        }
+
+        const threshold = this.lastCall.getTime() + this.timeout.inMilliseconds();
+        return Date.now() > threshold;
+    }
+}
