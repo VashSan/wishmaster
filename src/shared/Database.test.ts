616081df820a99c58d2c812a4a4bfaa86582aa78
Alexander Schwoch <alex@palc.de>
2020-02-05 22:13:19 +0100

    basic database rework

--- src/shared/Database.test.ts
+++ src/shared/Database.test.ts
@@ -1,42 +1,84 @@
-import { Database } from "./Database";
+import { Database, UserCollection, LogCollection } from "./Database";
 
-import Nedb = require("nedb");
+import { MockProxy, mock } from "jest-mock-extended";
+import { IConfiguration } from "./Configuration";
+import { ILogger } from "psst-log";
+import { Seconds } from "./Helper";
+
 jest.mock('nedb');
+import Nedb = require("nedb");
 
+let config: MockProxy<IConfiguration> & IConfiguration;
+let logger: MockProxy<ILogger> & ILogger;
 
-test('create collection', () => {
-    // Arrange
-    let db = new Database();
-    expect(db.getSize()).toBe(0);
+describe('Database', () => {
+    beforeEach(() => {
+        require("nedb");
 
-    // Act
-    db.createCollection("users", {
-        timestampData: true
-    }, () => { });
+        config = mock<IConfiguration>();
+        logger = mock<ILogger>();
+    });
 
-    // Assert
-    expect(Nedb.prototype.loadDatabase).toBeCalledTimes(1);
-    expect(db.getSize()).toBe(1);
-});
+    test('create collection', () => {
+        // Arrange
+        let db = new Database(config, logger);
+        expect(db.getSize()).toBe(0);
 
-test('return created collection', () => {
-    // Arrange
-    let db = new Database();
-    expect(db.getSize()).toBe(0);
+        // Act
+        db.createCollection(UserCollection, "users");
 
-    // Act
-    db.createCollection("users", { timestampData: true }, () => { });
-    db.createCollection("log", { timestampData: true }, () => { });
+        // Assert
+        expect(Nedb.prototype.loadDatabase).toBeCalledTimes(1);
+        expect(db.getSize()).toBe(1);
+    });
 
-    // Assert
-    expect(db.getSize()).toBe(2);
+    test('return created collection', () => {
+        // Arrange
+        let db = new Database(config, logger);
+        expect(db.getSize()).toBe(0);
 
-    expect(() => db.get("log")).not.toThrow();
-    expect(() => db.get("users")).not.toThrow();
-});
+        // Act
+        db.createCollection(UserCollection, "users");
+        db.createCollection(LogCollection, "log");
 
-test('use unavailable database throws', () => {
-    let db = new Database();
-    expect(() => db.get("log")).toThrow();
-    expect(() => db.get("users")).toThrow();
+        // Assert
+        expect(db.getSize()).toBe(2);
+
+        expect(() => db.get("log")).not.toThrow();
+        expect(() => db.get("users")).not.toThrow();
+    });
+
+    test('use unavailable database throws', () => {
+        let db = new Database(config, logger);
+        expect(() => db.get("log")).toThrow();
+        expect(() => db.get("users")).toThrow();
+    });
+
+    test('loading fails', async () => {
+        // Arrange
+        let db = new Database(config, logger);
+
+        // Act
+        db.createCollection(UserCollection, "users");
+
+        // Assert
+        await expect(db.waitAllLoaded(new Seconds(0.1))).rejects.toBeTruthy();
+    });
+
+    test('all loaded', async () => {
+        // Arrange
+        Nedb.prototype.loadDatabase = (cb) => {
+            if (cb) {
+                let nullParam: any = null
+                cb(nullParam); // fake successfull loading
+            }
+        };
+        let db = new Database(config, logger);
+
+        // Act
+        db.createCollection(UserCollection, "users");
+
+        // Assert
+        await expect(db.waitAllLoaded(new Seconds(0.1))).resolves.toBe(undefined);
+    });
 });
--- src/shared/Database.ts
+++ src/shared/Database.ts
@@ -1,10 +1,15 @@
 import Nedb = require("nedb");
 import { isNullOrUndefined } from "util";
+import { IConfiguration } from "./Configuration";
+import { ILogger, LogManager } from "psst-log";
+import { Seconds } from "./Helper";
 
 type CollectionMap = Map<string, ICollection>;
 
 export interface ICollection {
-
+    isInitialized(): boolean;
+    getError(): any;
+    getName(): string;
 }
 
 export interface IUserCollection extends ICollection {
@@ -17,44 +22,125 @@ export interface ILogCollection extends ICollection {
 
 export interface IDatabase {
     getSize(): number;
-    createCollection(name: string, options: object, cb: (err: any) => void): void;
+    createCollection<T extends ICollection>(type: { new(...args: any[]): T; }, name: string): T
     get(name: string): ICollection;
 }
 
 class Collection implements ICollection {
-    private db: Nedb;
-    constructor(db: Nedb) {
+    private readonly db: Nedb;
+    private readonly logger: ILogger;
+    private readonly name: string;
+
+    private initialized: boolean = false;
+    private error: any = null;
+
+
+    constructor(name: string, db: Nedb, logger?: ILogger) {
+        if (logger) {
+            this.logger = logger;
+        } else {
+            this.logger = LogManager.getLogger();
+        }
         this.db = db;
+        this.name = name;
+        db.loadDatabase(this.loadDatabaseCallback.bind(this));
+    }
+
+    public isInitialized(): boolean {
+        return this.initialized;
+    }
+
+    public getName(): string {
+        return this.name;
     }
 
 
+    public getError(): any {
+        return this.error;
+    }
+
+    private loadDatabaseCallback(err: any): void {
+        if (err != null) {
+            this.error = err;
+        } else {
+            this.initialized = true;
+        }
+    }
 }
 
 export class Database implements IDatabase {
-    private map: CollectionMap;
+    private readonly map: CollectionMap = new Map<string, ICollection>();
+    private config: IConfiguration;
+    private logger: ILogger;
 
-    constructor() {
-        this.map = new Map<string, ICollection>();
+    constructor(config: IConfiguration, logger?: ILogger) {
+        this.config = config;
+        if (logger) {
+            this.logger = logger;
+        } else {
+            this.logger = LogManager.getLogger();
+        }
     }
 
     public getSize(): number {
         return this.map.size;
     }
 
-    public get(name: string) {
+    public get<T extends ICollection>(name: string): T {
         let collection = this.map.get(name);
         if (isNullOrUndefined(collection)) {
             // we throw because we implemented that this could never happen
             throw `Collection '${collection}' is not initialized`;
         }
+        return <T>collection;
+    }
+
+    public createCollection<T extends ICollection>(type: { new(...args: any[]): T; }, name: string): T {
+        const configDir = this.config.getConfigDir();
+        const channel = this.config.getChannel();
+        const file = `${configDir}\\${name}-${channel}.db`
+
+        const options =
+        {
+            filename: file,
+            timestampData: true
+        };
+
+        let db = new Nedb(options);
+        let collection = new type(name, db);
+
+        this.map.set(name, collection);
         return collection;
     }
 
-    public createCollection(name: string, options: object, cb: (err: any) => void): void {
-        let db = new Nedb(options);
-        db.loadDatabase(cb);
+    public waitAllLoaded(timeoutInSeconds: Seconds): Promise<void> {
+        return new Promise((resolve, reject) => {
+            let done = false;
+            let start = Date.now();
+            
+            let interval = setInterval(()=>{
+                done = true;
+                this.map.forEach(element => {
+                    done = done && element.isInitialized();
+                    if (element.getError() != null) {
+                        reject(`Database '${element.getName()}' failed to load: ${element.getError()}`);
+                    }
+                });
 
-        let collection = new Collection(db);
-        this.map.set(name, collection);
+                let timedOut = Date.now() > start + timeoutInSeconds.inMilliseconds();
+
+                if (timedOut || done) {
+                    clearInterval(interval);
+                    if (done){
+                        resolve();
+                    } else {
+                        reject("Timeout reached");
+                    }
+                }
+            }, new Seconds(0.1).inMilliseconds());
+
+        });
     }
 }
+export class UserCollection extends Collection { };
+export class LogCollection extends Collection { };
