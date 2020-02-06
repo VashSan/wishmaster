504026cb38c4a517fd80a438b768d5f31bcfaf3a
Alexander Schwoch <alex@palc.de>
2020-02-06 23:59:28 +0100

    write unhandled rejections to console

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
 
-import {Startup} from "./src/Startup"
+import { Startup } from "./src/Startup"
+
+process.on('unhandledRejection', error => {
+    console.error('unhandledRejection: ');
+    console.log(error);
+});
 
 const startup = new Startup();
 startup.main();
