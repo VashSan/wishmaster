a14e6486d3791f4d9b1940f9468718943be7d6af
Alexander Schwoch <alex@palc.de>
2020-02-08 15:01:03 +0100

    added alerts test

--- src/Features/Alerts.test.ts
+++ src/Features/Alerts.test.ts
@@ -1,11 +1,13 @@
-import { IAlert, IConfiguration, IContext, IUserCollection, IDatabase, IObsController, ViewerAction, IEmail, IEmailAccess } from "../shared";
+import { IAlert, IConfiguration, IContext, IUserCollection, IDatabase, IObsController, ViewerAction, IEmail, IEmailAccess, IMediaPlayer } from "../shared";
 import { mock, MockProxy } from "jest-mock-extended";
 import Alerts from "./Alerts";
 import { ILogger } from "psst-log";
 
 const bob = "bob";
+const twitch = "jtv";
 let userDb: MockProxy<IUserCollection> & IUserCollection;
 let obs: MockProxy<IObsController> & IObsController;
+let mediaPlayer: MockProxy<IMediaPlayer> & IMediaPlayer;
 
 function getDatabaseMock(): MockProxy<IDatabase> & IDatabase {
     let db = mock<IDatabase>();
@@ -14,6 +16,8 @@ function getDatabaseMock(): MockProxy<IDatabase> & IDatabase {
 }
 
 function getContextMock(): MockProxy<IContext> & IContext {
+    mediaPlayer = mock<IMediaPlayer>();
+    
     let alert = mock<IAlert>();
     alert.sceneTextPattern = "Thanks {0}";
     alert.bannerTextPattern = "Hey {0}";
@@ -33,7 +37,7 @@ function getContextMock(): MockProxy<IContext> & IContext {
 
     let email = mock<IEmailAccess>();
     context.getEmail.mockReturnValue(email);
-
+    context.getMediaPlayer.mockReturnValue(mediaPlayer);
     context.getObs.mockReturnValue(obs);
 
     return context;
@@ -63,7 +67,7 @@ test('handle alert command', (done) => {
 
     // Act
     let alerts = new Alerts(context, logger);
-    alerts.setup(() => setTimeout(() => {+
+    alerts.setup(() => setTimeout(() => {
         // Assert
         expect(userDb.newFollowFrom).toBeCalledTimes(1);
         expect(obs.setText).toBeCalledTimes(2); // 1. banner 2. alert
@@ -72,4 +76,25 @@ test('handle alert command', (done) => {
 
     alerts.act({ channel: "#mine", from: bob, text: "!alert follower alice" });
 
+});
+
+test('host', (done) => {
+    // Arrange
+    let logger = mock<ILogger>();
+    let context = getContextMock();
+
+    userDb.findLastActions.mockResolvedValue([{ name: bob, lastAction: ViewerAction.Follow, lastActionDate: new Date() }]);
+
+    // Act
+    let alerts = new Alerts(context, logger);
+    
+    alerts.setup(() => setTimeout(() => {
+        // Assert
+        expect(userDb.newHostFrom).toBeCalledTimes(1);
+        expect(mediaPlayer.play).toBeCalledTimes(1);
+        expect(obs.setText).toBeCalledTimes(1);
+        done();
+    }, 100));
+
+    alerts.act({ channel: "#bob", from: twitch, text: "alice is now hosting you." });
 });
