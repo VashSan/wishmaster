b8cc00b6765c3fc432206955c8cb681f0b8e2f66
Alexander Schwoch <alex@palc.de>
2020-02-16 11:38:01 +0100

    parse spotify track urls and track ids

--- src/Features/SongRequest.ts
+++ src/Features/SongRequest.ts
@@ -101,53 +101,83 @@ export class SongRequest extends FeatureBase implements ISongRequest {
             return;
         }
 
+        this.api.setAccessToken(token);
         if (cmd == "!sr" || cmd == "!songrequest") {
-            this.api.setAccessToken(token);
-
-            this.api.getMyCurrentPlaybackState().then((state) => {
-                if (!state.body.is_playing) {
-                    return this.api.searchTracks(request);
-                }
-            }).then((result) => {
-                if (result != undefined) {
-                    return result.body.tracks?.items[0].uri;
-                }
-            }).then((trackid) => {
-                if (trackid) {
-                    return this.api.play({ uris: [trackid] });
-                }
-            }).catch((err) => {
-                this.logger.warn("[!sr] Could not retrieve song info.", JSON.stringify(err), JSON.stringify(msg));
-                const r = this.createResponse(`Sorry @${msg.from} - your song was not found!`)
-                this.sendResponse(r);
-            });
-
+            this.requestSong(request, msg);
         } else if (cmd == "!song") {
-            this.api.setAccessToken(token);
-
-            this.api.getMyCurrentPlaybackState().then((state) => {
-                if (state.body.is_playing) {
-                    return this.api.getMyCurrentPlayingTrack();
-                }
-            }).then((result) => {
-                if (result) {
-                    const v = result.body.item?.name.toString();
-                    const trackName = v || "";
-
-                    if (trackName != "") {
-                        const r = this.createResponse(`Current song: ${trackName}`);
-                        this.sendResponse(r);
-                    }
-                }
-            }).catch((err) => {
-                // What could possibly go wrong?
-                this.logger.error("[!song] Could not retrieve song info.", JSON.stringify(err), JSON.stringify(msg));
-            });
-
-
+            this.requestCurrentSongInfo(msg);
         }
     }
 
+    private requestSong(request: string, msg: IMessage) {
+        const spotifyRegex = /spotify:track:([A-Za-z0-9]+)|https:\/\/open\.spotify\.com\/track\/([A-Za-z0-9]+)/;
+        const result = spotifyRegex.exec(request);
+        if (result != null && result.length > 1) {
+            let songId = result[1] ? result[1] : result[2];
+            this.requestSongById(songId, msg);
+        } else {
+            this.requestSongByName(request, msg);
+        }
+    }
 
+    private requestSongByName(request: string, msg: IMessage) {
+        this.api.getMyCurrentPlaybackState()
+            .then((state) => {
+                if (!state.body.is_playing) {
+                    return this.api.searchTracks(request);
+                }
+            })
+            .then((result) => {
+                if (result != undefined) {
+                    return result.body.tracks?.items[0].uri;
+                }
+            })
+            .then((trackid) => this.playTrack(trackid))
+            .catch((err) => this.handleRequestError(msg, err));
+    }
+
+    private requestSongById(songId: string, msg: IMessage) {
+        this.api.getMyCurrentPlaybackState()
+            .then((state) => {
+                if (!state.body.is_playing) {
+                    return this.api.getTrack(songId);
+                }
+            })
+            .then((track) => this.playTrack(track?.body.uri))
+            .catch((err) => this.handleRequestError(msg, err));
+    }
+
+    private playTrack(uri?: string) {
+        if (uri) {
+            return this.api.play({ uris: [uri] });
+        }
+    }
+
+    private handleRequestError(msg: IMessage, err: any) {
+        this.logger.warn("[!sr] Could not retrieve song.", JSON.stringify(err), JSON.stringify(msg));
+        const r = this.createResponse(`Sorry @${msg.from} - your song was not found!`)
+        this.sendResponse(r);
+    }
+
+    private requestCurrentSongInfo(msg: IMessage) {
+        this.api.getMyCurrentPlaybackState().then((state) => {
+            if (state.body.is_playing) {
+                return this.api.getMyCurrentPlayingTrack();
+            }
+        }).then((result) => {
+            if (result) {
+                const v = result.body.item?.name.toString();
+                const trackName = v || "";
+
+                if (trackName != "") {
+                    const r = this.createResponse(`Current song: ${trackName}`);
+                    this.sendResponse(r);
+                }
+            }
+        }).catch((err) => {
+            // What could possibly go wrong?
+            this.logger.error("[!song] Could not retrieve song info.", JSON.stringify(err), JSON.stringify(msg));
+        });
+    }
 
 }
